73c3d962c2e42b65b03362247a7b1ff77213cf3a
Winston <you@example.com>
2026-04-18 09:42:09 +0530

    working version before UI improvements

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gqihrpab7so"
+    "revision": "0.a019d1c6nuo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
